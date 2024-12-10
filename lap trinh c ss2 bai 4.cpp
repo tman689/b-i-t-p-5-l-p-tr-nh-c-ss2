0aa5e091b464e2b3f094d0087decc189606a9c75
tman689 <tranminhan05092000@gmail.com>
2024-12-10 21:15:25 +0700

    hoàn thành bài tập 5

---
+++ lap trinh c ss2 bai 4.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	int cd = 5;
+	int cr = 10;
+	
+	int dt = cd *cr;
+	int cv = 2 * (cd+cr);
+	
+	printf("Dien tich hinh chu nhat : %d\n", dt);
+	printf("Chu vi hinh chu nhat : %d\n", cv);
+	
+	return 0;
+	
+}
+	
